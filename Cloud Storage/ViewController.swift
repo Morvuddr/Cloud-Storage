5c22638c73dce7d12624e21148d61650662d4762
Igor Bopp <igor_bopp@mail.ru>
2019-06-30 14:26:13 +0300

    Initial Commit

---
+++ Cloud Storage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cloud Storage
+//
+//  Created by Игорь Бопп on 30/06/2019.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
